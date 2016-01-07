4c8e974aa7000a00109fd29d5466ddcf9be74053
Bjorn Johnson <bjornkjohnson@gmail.com>
2016-01-07 11:27:00 -0500

    initial commit

---
+++ test/com/bjorn/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package com.bjorn;
+
+import org.junit.Test;
+
+/**
+ * Created by bjornjohnson on 1/7/16.
+ */
+public class BowlingGameTest {
+    @Test
+    public void testInstanceOf(){
+        
+    }
+}
